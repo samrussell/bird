4e2932951c1461938fce448bdbb44da1f8ecdadf
Sam Russell <sam.h.russell@gmail.com>
2013-06-14 11:20:11 +1200

    didn't replace all vars

--- bird-1.3.10/nest/iface.c
+++ bird-1.3.10/nest/iface.c
@@ -149,7 +149,7 @@ ifa_send_notify(struct proto *p, unsigned c, struct ifa *a)
       // but IS-IS is the best routing protocol ever
       // print some debug so we can confirm that this is where interface updates get sent to router engine from
       log_msg(L_DEBUG "ifa_send_notify() called");
-      log_msg(L_DEBUG "Added route to %u.%u.%u.%u/%d", ((unsigned char *)(&(a->prefix)))[3], ((unsigned char *)(&(a->prefix)))[2], ((unsigned char *)(&(a->prefix)))[1], ((unsigned char *)(&(a->prefix)))[0], n->n.pxlen);
+      log_msg(L_DEBUG "Added route to %u.%u.%u.%u/%d", ((unsigned char *)(&(a->prefix)))[3], ((unsigned char *)(&(a->prefix)))[2], ((unsigned char *)(&(a->prefix)))[1], ((unsigned char *)(&(a->prefix)))[0], a->pxlen);
       if(a->iface){
         log_msg(L_DEBUG "Interface: %s", a->iface->name);
       }
