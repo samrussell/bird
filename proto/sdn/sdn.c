ca9d5fde2b3515af10b29ad16bace8dbe5b36781
Sam Russell <sam.h.russell@gmail.com>
2013-06-17 20:24:18 +1200

    Renamed stuff for a template to build off

--- proto/sdn/sdn.c
+++ proto/sdn/sdn.c
@@ -23,12 +23,12 @@
 #include "filter/filter.h"
 #include "lib/string.h"
 
-#include "pipe.h"
+#include "sdn.h"
 
 static void
-pipe_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, ea_list *attrs)
+sdn_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, ea_list *attrs)
 {
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_proto *p = (struct sdn_proto *) P;
   struct announce_hook *ah = (src_table == P->table) ? p->peer_ahook : P->main_ahook;
   rtable *dst_table = ah->table;
   struct proto *src;
@@ -40,7 +40,7 @@ pipe_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, e
   if (!new && !old)
     return;
 
-  if (dst_table->pipe_busy)
+  if (dst_table->sdn_busy)
     {
       log(L_ERR "Pipe loop detected when sending %I/%d to table %s",
 	  n->n.prefix, n->n.pxlen, dst_table->name);
@@ -52,10 +52,10 @@ pipe_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, e
     {
       memcpy(&a, new->attrs, sizeof(rta));
 
-      if (p->mode == PIPE_OPAQUE)
+      if (p->mode == SDN_OPAQUE)
 	{
 	  a.proto = &p->p;
-	  a.source = RTS_PIPE;
+	  a.source = RTS_SDN;
 	}
 
       a.aflags = 0;
@@ -65,7 +65,7 @@ pipe_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, e
       e->net = nn;
       e->pflags = 0;
 
-      if (p->mode == PIPE_TRANSPARENT)
+      if (p->mode == SDN_TRANSPARENT)
 	{
 	  /* Copy protocol specific embedded attributes. */
 	  memcpy(&(e->u), &(new->u), sizeof(e->u));
@@ -81,13 +81,13 @@ pipe_rt_notify(struct proto *P, rtable *src_table, net *n, rte *new, rte *old, e
       src = old->attrs->proto;
     }
 
-  src_table->pipe_busy = 1;
-  rte_update2(ah, nn, e, (p->mode == PIPE_OPAQUE) ? &p->p : src);
-  src_table->pipe_busy = 0;
+  src_table->sdn_busy = 1;
+  rte_update2(ah, nn, e, (p->mode == SDN_OPAQUE) ? &p->p : src);
+  src_table->sdn_busy = 0;
 }
 
 static int
-pipe_import_control(struct proto *P, rte **ee, ea_list **ea UNUSED, struct linpool *p UNUSED)
+sdn_import_control(struct proto *P, rte **ee, ea_list **ea UNUSED, struct linpool *p UNUSED)
 {
   struct proto *pp = (*ee)->sender->proto;
 
@@ -97,12 +97,12 @@ pipe_import_control(struct proto *P, rte **ee, ea_list **ea UNUSED, struct linpo
 }
 
 static int
-pipe_reload_routes(struct proto *P)
+sdn_reload_routes(struct proto *P)
 {
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_proto *p = (struct sdn_proto *) P;
 
   /*
-   * Because the pipe protocol feeds routes from both routing tables
+   * Because the sdn protocol feeds routes from both routing tables
    * together, both directions are reloaded during refeed and 'reload
    * out' command works like 'reload' command. For symmetry, we also
    * request refeed when 'reload in' command is used.
@@ -116,29 +116,29 @@ pipe_reload_routes(struct proto *P)
 }
 
 static struct proto *
-pipe_init(struct proto_config *C)
+sdn_init(struct proto_config *C)
 {
-  struct pipe_config *c = (struct pipe_config *) C;
-  struct proto *P = proto_new(C, sizeof(struct pipe_proto));
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_config *c = (struct sdn_config *) C;
+  struct proto *P = proto_new(C, sizeof(struct sdn_proto));
+  struct sdn_proto *p = (struct sdn_proto *) P;
 
   p->mode = c->mode;
   p->peer_table = c->peer->table;
-  P->accept_ra_types = (p->mode == PIPE_OPAQUE) ? RA_OPTIMAL : RA_ANY;
-  P->rt_notify = pipe_rt_notify;
-  P->import_control = pipe_import_control;
-  P->reload_routes = pipe_reload_routes;
+  P->accept_ra_types = (p->mode == SDN_OPAQUE) ? RA_OPTIMAL : RA_ANY;
+  P->rt_notify = sdn_rt_notify;
+  P->import_control = sdn_import_control;
+  P->reload_routes = sdn_reload_routes;
 
   return P;
 }
 
 static int
-pipe_start(struct proto *P)
+sdn_start(struct proto *P)
 {
-  struct pipe_config *cf = (struct pipe_config *) P->cf;
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_config *cf = (struct sdn_config *) P->cf;
+  struct sdn_proto *p = (struct sdn_proto *) P;
 
-  /* Lock both tables, unlock is handled in pipe_cleanup() */
+  /* Lock both tables, unlock is handled in sdn_cleanup() */
   rt_lock_table(P->table);
   rt_lock_table(p->peer_table);
 
@@ -159,9 +159,9 @@ pipe_start(struct proto *P)
 }
 
 static void
-pipe_cleanup(struct proto *P)
+sdn_cleanup(struct proto *P)
 {
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_proto *p = (struct sdn_proto *) P;
 
   bzero(&P->stats, sizeof(struct proto_stats));
   bzero(&p->peer_stats, sizeof(struct proto_stats));
@@ -174,9 +174,9 @@ pipe_cleanup(struct proto *P)
 }
 
 static void
-pipe_postconfig(struct proto_config *C)
+sdn_postconfig(struct proto_config *C)
 {
-  struct pipe_config *c = (struct pipe_config *) C;
+  struct sdn_config *c = (struct sdn_config *) C;
 
   if (!c->peer)
     cf_error("Name of peer routing table not specified");
@@ -192,12 +192,12 @@ pipe_postconfig(struct proto_config *C)
 extern int proto_reconfig_type;
 
 static int
-pipe_reconfigure(struct proto *P, struct proto_config *new)
+sdn_reconfigure(struct proto *P, struct proto_config *new)
 {
-  struct pipe_proto *p = (struct pipe_proto *)P;
+  struct sdn_proto *p = (struct sdn_proto *)P;
   struct proto_config *old = P->cf;
-  struct pipe_config *oc = (struct pipe_config *) old;
-  struct pipe_config *nc = (struct pipe_config *) new;
+  struct sdn_config *oc = (struct sdn_config *) old;
+  struct sdn_config *nc = (struct sdn_config *) new;
 
   if ((oc->peer->table != nc->peer->table) || (oc->mode != nc->mode))
     return 0;
@@ -227,36 +227,36 @@ pipe_reconfigure(struct proto *P, struct proto_config *new)
 }
 
 static void
-pipe_copy_config(struct proto_config *dest, struct proto_config *src)
+sdn_copy_config(struct proto_config *dest, struct proto_config *src)
 {
   /* Just a shallow copy, not many items here */
-  proto_copy_rest(dest, src, sizeof(struct pipe_config));
+  proto_copy_rest(dest, src, sizeof(struct sdn_config));
 }
 
 static void
-pipe_get_status(struct proto *P, byte *buf)
+sdn_get_status(struct proto *P, byte *buf)
 {
-  struct pipe_proto *p = (struct pipe_proto *) P;
+  struct sdn_proto *p = (struct sdn_proto *) P;
 
-  bsprintf(buf, "%c> %s", (p->mode == PIPE_OPAQUE) ? '-' : '=', p->peer_table->name);
+  bsprintf(buf, "%c> %s", (p->mode == SDN_OPAQUE) ? '-' : '=', p->peer_table->name);
 }
 
 static void
-pipe_show_stats(struct pipe_proto *p)
+sdn_show_stats(struct sdn_proto *p)
 {
   struct proto_stats *s1 = &p->p.stats;
   struct proto_stats *s2 = &p->peer_stats;
 
   /*
-   * Pipe stats (as anything related to pipes) are a bit tricky. There
+   * Pipe stats (as anything related to sdns) are a bit tricky. There
    * are two sets of stats - s1 for ahook to the primary routing and
    * s2 for the ahook to the secondary routing table. The user point
    * of view is that routes going from the primary routing table to
    * the secondary routing table are 'exported', while routes going in
    * the other direction are 'imported'.
    *
-   * Each route going through a pipe is, technically, first exported
-   * to the pipe and then imported from that pipe and such operations
+   * Each route going through a sdn is, technically, first exported
+   * to the sdn and then imported from that sdn and such operations
    * are counted in one set of stats according to the direction of the
    * route propagation. Filtering is done just in the first part
    * (export). Therefore, we compose stats for one directon for one
@@ -286,10 +286,10 @@ pipe_show_stats(struct pipe_proto *p)
 }
 
 static void
-pipe_show_proto_info(struct proto *P)
+sdn_show_proto_info(struct proto *P)
 {
-  struct pipe_proto *p = (struct pipe_proto *) P;
-  struct pipe_config *cf = (struct pipe_config *) P->cf;
+  struct sdn_proto *p = (struct sdn_proto *) P;
+  struct sdn_config *cf = (struct sdn_config *) P->cf;
 
   // cli_msg(-1006, "  Table:          %s", P->table->name);
   // cli_msg(-1006, "  Peer table:     %s", p->peer_table->name);
@@ -301,21 +301,21 @@ pipe_show_proto_info(struct proto *P)
   proto_show_limit(cf->c.out_limit, "Export limit:");
 
   if (P->proto_state != PS_DOWN)
-    pipe_show_stats(p);
+    sdn_show_stats(p);
 }
 
 
-struct protocol proto_pipe = {
+struct protocol proto_sdn = {
   name:			"Pipe",
-  template:		"pipe%d",
+  template:		"sdn%d",
   multitable:		1,
-  preference:		DEF_PREF_PIPE,
-  postconfig:		pipe_postconfig,
-  init:			pipe_init,
-  start:		pipe_start,
-  cleanup:		pipe_cleanup,
-  reconfigure:		pipe_reconfigure,
-  copy_config:  	pipe_copy_config,
-  get_status:		pipe_get_status,
-  show_proto_info:	pipe_show_proto_info
+  preference:		DEF_PREF_SDN,
+  postconfig:		sdn_postconfig,
+  init:			sdn_init,
+  start:		sdn_start,
+  cleanup:		sdn_cleanup,
+  reconfigure:		sdn_reconfigure,
+  copy_config:  	sdn_copy_config,
+  get_status:		sdn_get_status,
+  show_proto_info:	sdn_show_proto_info
 };
